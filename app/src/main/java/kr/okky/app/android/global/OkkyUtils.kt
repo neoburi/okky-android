48b2b9949e096e22f63d631d34cd6a21f8246816
IK.Choi <im.kugie@neoburi.com>
2018-08-23 08:42:37 +0900

    file 관련 메소드 추가

--- app/src/main/java/kr/okky/app/android/global/OkkyUtils.kt
+++ app/src/main/java/kr/okky/app/android/global/OkkyUtils.kt
@@ -1,10 +1,9 @@
 package kr.okky.app.android.global
 
 import android.content.Context
-import java.io.File
-import java.io.FileOutputStream
-import java.io.IOException
-import java.io.InputStream
+import android.content.res.AssetManager
+import java.io.*
+
 
 object OkkyUtils {
     const val menuFile = "drawerMenu.json"
@@ -30,6 +29,32 @@ object OkkyUtils {
         return false
     }
 
+    fun readMenuJsonFromAssets(assetManager: AssetManager): String {
+        var contents = StringBuilder()
+        var input: InputStream? = null
+        var reader: BufferedReader? = null
+        try {
+            input = assetManager.open(menuFile)
+            reader = BufferedReader(InputStreamReader(input))
+            while (true) {
+                val line = reader.readLine()
+                if(line == null) {
+                    break
+                }else{
+                    contents.append(line)
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            try {
+                input?.close()
+                reader?.close()
+            } catch (e: IOException) {}
+        }
+        return contents.toString()
+    }
+
     fun copyTo(inputStream: InputStream, target: File): Boolean {
         val out = FileOutputStream(target)
         try {
@@ -51,5 +76,28 @@ object OkkyUtils {
             out.close()
         }
         return false
+    }
+
+    fun loadRaw(context: Context, rawId: Int): String {
+        val inputData = context.resources.openRawResource(rawId)
+        val stringBuilder = StringBuilder()
+        var bufferedReader:BufferedReader? = null
+        try {
+            bufferedReader = BufferedReader(InputStreamReader(inputData, "UTF-8"))
+            while (true) {
+                val str = bufferedReader.readLine()
+                if(!str.isNullOrEmpty()) {
+                    stringBuilder.append(str).append("\n")
+                }else{
+                    break
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }finally {
+            bufferedReader?.close()
+        }
+
+        return stringBuilder.toString()
     }
 }
