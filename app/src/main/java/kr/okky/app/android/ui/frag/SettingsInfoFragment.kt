a10295fdb73ba537a2673c0c7d6c7c5131cfad60
IK.Choi <im.kugie@neoburi.com>
2018-08-28 11:52:55 +0900

    add scroll movement method to textview

--- app/src/main/java/kr/okky/app/android/ui/frag/SettingsInfoFragment.kt
+++ app/src/main/java/kr/okky/app/android/ui/frag/SettingsInfoFragment.kt
@@ -1,5 +1,6 @@
 package kr.okky.app.android.ui.frag
 
+import android.text.method.ScrollingMovementMethod
 import android.widget.TextView
 import kr.okky.app.android.R
 import kr.okky.app.android.utils.OkkyUtils
@@ -7,9 +8,9 @@ import kr.okky.app.android.utils.OkkyUtils
 class SettingsInfoFragment : BaseFragment(){
 
     override fun followingWorksAfterInflateView() {
-        val version = getView<TextView>(R.id.txt_version)
-        version.text =
+        getView<TextView>(R.id.txt_version).text =
                 String.format(getString(R.string.txt_app_version), OkkyUtils.getVersionName(activity!!))
+        getView<TextView>(R.id.txt_thanks_desc).movementMethod = ScrollingMovementMethod()
     }
 
     override fun getViewResourceId(): Int = R.layout.fragment_setting_info
